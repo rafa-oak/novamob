55a591c119d2c1f6fe1866352e0f59f504057e11
Rafael Carvalho <rafascarvalho1211@gmail.com>
2024-07-09 15:40:53 +0100

    Scout gz classic done

--- scout_description/launch/display.launch.py
+++ scout_description/launch/display.launch.py
@@ -5,19 +5,23 @@ import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='scout_description').find('scout_description')
-    default_model_path = os.path.join(pkg_share, 'urdf/scout_v2/scout_v2.urdf')
+    default_model_path = os.path.join(pkg_share, 'urdf/scout_v2/scout_v2.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     world_path=os.path.join(pkg_share, 'world/my_world.sdf')
+    
+    
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')]), 'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         arguments=[default_model_path],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+
     )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -25,6 +29,7 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
     spawn_entity = launch_ros.actions.Node(
         package='gazebo_ros',
@@ -44,9 +49,10 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
+
         joint_state_publisher_node,
         #joint_state_publisher_gui_node,
         robot_state_publisher_node,
