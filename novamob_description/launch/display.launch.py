3245269c3415d539f40f4c81d2ab1678af2a29af
Rafael Carvalho <rafascarvalho1211@gmail.com>
2024-07-15 16:21:53 +0100

    Added indoor_world testing

--- novamob_description/launch/display.launch.py
+++ novamob_description/launch/display.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='novamob_description').find('novamob_description')
     default_model_path = os.path.join(pkg_share, 'src/description/novamob_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-    world_path=os.path.join(pkg_share, 'world/outdoor.world')
+    world_path=os.path.join(pkg_share, 'world/indoor_2.world')
 
 
     robot_state_publisher_node = launch_ros.actions.Node(
