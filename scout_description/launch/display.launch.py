10d25a852ef5a2a34a05e566d243fcc5e474a65c
Rafael Carvalho <rafascarvalho1211@gmail.com>
2024-10-31 14:38:02 +0000

    Small change to gz classic testing in scout

--- scout_description/launch/display.launch.py
+++ scout_description/launch/display.launch.py
@@ -5,7 +5,7 @@ import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='scout_description').find('scout_description')
-    default_model_path = os.path.join(pkg_share, 'urdf/scout_v2/scout_v2_trailer.xacro')
+    default_model_path = os.path.join(pkg_share, 'urdf/scout_v2/scout_v2.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     world_path=os.path.join(pkg_share, 'world/indoor_2.world')
     
