7a583bfe19319a64c861398f0f62a3faf1386b40
Rafael Carvalho <rafascarvalho1211@gmail.com>
2024-11-11 15:46:03 +0000

    Added some test files to scout

---
+++ scout_nav2_gz/launch/display.launch_diff.py
@@ -0,0 +1,337 @@
+import launch
+from launch.actions import (
+    ExecuteProcess,
+    DeclareLaunchArgument,
+    RegisterEventHandler,
+    SetEnvironmentVariable,
+)
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    NotSubstitution,
+    AndSubstitution,
+    PythonExpression,
+
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import launch_ros
+import os
+
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(
+        package="scout_nav2_gz"
+    ).find("scout_nav2_gz")
+
+    # Paths to URDF files
+    default_model_path = os.path.join(pkg_share, "urdf/scout_v2/scout_v2.xacro")
+    trailer_model_path = os.path.join(pkg_share, "urdf/scout_v2/scout_v2_trailer.xacro")
+
+    # RViz and world file paths
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/urdf_config.rviz")
+    default_world_path = os.path.join(pkg_share, "world/ign_indoor/ign_indoor_fixed.sdf")
+    gz_models_path = os.path.join(pkg_share, "models")
+
+    # Launch configurations
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_localization = LaunchConfiguration("use_localization")
+    use_rviz = LaunchConfiguration("use_rviz")
+    use_trailer = LaunchConfiguration("use_trailer")
+    log_level = LaunchConfiguration("log_level")
+    gz_verbosity = LaunchConfiguration("gz_verbosity")
+    run_headless = LaunchConfiguration("run_headless")
+    world_path = LaunchConfiguration("world")  
+
+
+    # robot_state_publisher_node = Node(
+    #     condition=IfCondition(PythonExpression(["'", use_trailer, "' == 'False'"])),
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     parameters=[
+    #         {"robot_description": Command(["xacro ", LaunchConfiguration("model")])}
+    #     ],
+    # )
+
+    # robot_state_publisher_node_trailer = Node(
+    #     condition=IfCondition(PythonExpression(["'", use_trailer, "' == 'True'"])),
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     parameters=[
+    #         {"robot_description": Command(["xacro ", trailer_model_path])}
+    #     ],
+    # )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {
+                "robot_description": Command([
+                    "xacro ",
+                    PythonExpression([
+                        "'", trailer_model_path, "' if '", LaunchConfiguration("use_trailer"), "' == 'True' else '", LaunchConfiguration("model"), "'"
+                    ])
+                ])
+            }
+        ],
+    )
+
+    rviz_node = Node(
+        condition=IfCondition(AndSubstitution(NotSubstitution(run_headless), use_rviz)),
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
+    )
+
+    # Localize using odometry and IMU data. 
+    # It can be turned off because the navigation stack uses AMCL with lidar data for localization
+    robot_localization_node = Node(
+        condition=launch.conditions.IfCondition(use_localization),
+        package="robot_localization",
+        executable="ekf_node",
+        name="ekf_filter_node",
+        output="screen",
+        parameters=[
+            os.path.join(pkg_share, "config/ekf.yaml"),
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    # gazebo have to be executed with shell=False, or test_launch won't terminate it
+    #   see: https://github.com/ros2/launch/issues/545
+    # This code is form taken ros_gz_sim and modified to work with shell=False
+    #   see: https://github.com/gazebosim/ros_gz/blob/ros2/ros_gz_sim/launch/gz_sim.launch.py.in
+    gz_env = {'GZ_SIM_SYSTEM_PLUGIN_PATH':
+           ':'.join([os.environ.get('GZ_SIM_SYSTEM_PLUGIN_PATH', default=''),
+                     os.environ.get('LD_LIBRARY_PATH', default='')]),
+           'IGN_GAZEBO_SYSTEM_PLUGIN_PATH':  # TODO(CH3): To support pre-garden. Deprecated.
+                      ':'.join([os.environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', default=''),
+                                os.environ.get('LD_LIBRARY_PATH', default='')])}
+    gazebo = [
+        ExecuteProcess(
+            condition=launch.conditions.IfCondition(run_headless),
+            cmd=['ruby', FindExecutable(name="ign"), 'gazebo',  '-r', '-v', gz_verbosity, '-s', '--headless-rendering', world_path],
+            output='screen',
+            additional_env=gz_env, # type: ignore
+            shell=False,
+        ),
+        ExecuteProcess(
+            condition=launch.conditions.UnlessCondition(run_headless),
+            cmd=['ruby', FindExecutable(name="ign"), 'gazebo',  '-r', '-v', gz_verbosity, world_path],
+            output='screen',
+            additional_env=gz_env, # type: ignore
+            shell=False,
+        )
+    ]
+
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-name",
+            "scout",
+            "-topic",
+            "robot_description",
+            "-z",
+            "1.0",
+            "-x",
+            "-2.0",
+            "--ros-args",
+            "--log-level",
+            log_level,
+        ],
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
+
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan",
+            "/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU",
+            "/sky_cam@sensor_msgs/msg/Image@ignition.msgs.Image",
+            "/depth_camera@sensor_msgs/msg/Image@ignition.msgs.Image",
+            "/robot_cam@sensor_msgs/msg/Image@ignition.msgs.Image",
+            "/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
+            "/navsat@sensor_msgs/msg/NavSatFix[ignition.msgs.NavSat",
+            '/odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
+            "cmd_vel@geometry_msgs/msg/Twist[ignition.msgs.Twist",
+            '/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+
+            # Clock message is necessary for the diff_drive_controller to accept commands https://github.com/ros-controls/gz_ros2_control/issues/106
+            "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
+        ],
+        remappings=[
+            ("/navsat", "/gps/fix"),  # Remapping the Ignition GPS topic to the ROS /gps/fix topic
+            #("/diff_drive_base_controller/odom", "/odom")
+
+        ],
+        output="screen",
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        name="activate_joint_state_broadcaster",
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "joint_state_broadcaster",
+        ],
+        shell=False,
+        output="screen",
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        name="activate_diff_drive_base_controller",
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "diff_drive_base_controller",
+        ],
+        shell=False,
+        output="screen",
+    )
+
+    # Only launch joint_state_publisher_gui if 'use_trailer' is True
+    joint_state_publisher_gui_node = launch_ros.actions.Node(
+        condition=IfCondition(use_trailer),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+    )
+
+    relay_odom = Node(
+        name="relay_odom",
+        package="topic_tools",
+        executable="relay",
+        parameters=[
+            {
+                "input_topic": "/diff_drive_base_controller/odom",
+                "output_topic": "/odom",
+            }
+        ],
+        output="screen",
+    )
+
+    relay_cmd_vel = Node(
+        name="relay_cmd_vel",
+        package="topic_tools",
+        executable="relay",
+        parameters=[
+            {
+                "input_topic": "/cmd_vel",
+                "output_topic": "/diff_drive_base_controller/cmd_vel_unstamped",
+            }
+        ],
+        output="screen",
+    )
+    map_static_tf = Node(package='tf2_ros',
+                        executable='static_transform_publisher',
+                        name='static_transform_publisher',
+                        output='log',
+                        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'map', 'odom']
+    )
+
+
+    return launch.LaunchDescription(
+        [
+            SetEnvironmentVariable(
+                name="IGN_GAZEBO_RESOURCE_PATH",
+                value=gz_models_path,
+            ),
+            SetEnvironmentVariable(
+                name="IGN_GAZEBO_MODEL_PATH",
+                value=gz_models_path,
+            ),
+            DeclareLaunchArgument(
+                name="model",
+                default_value=default_model_path,
+                description="Absolute path to robot urdf file",
+            ),
+            DeclareLaunchArgument(
+                name="use_trailer", 
+                default_value="False",
+                description="Use the robot model with a trailer",
+            ),
+            DeclareLaunchArgument(
+                name="use_rviz",
+                default_value="True",
+                description="Start RViz",
+            ),
+            DeclareLaunchArgument(
+                name="run_headless",
+                default_value="False",
+                description="Start GZ in hedless mode and don't start RViz (overrides use_rviz)",
+            ),
+            DeclareLaunchArgument(
+                name="rvizconfig",
+                default_value=default_rviz_config_path,
+                description="Absolute path to rviz config file",
+            ),
+            DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="True",
+                description="Flag to enable use_sim_time",
+            ),
+            DeclareLaunchArgument(
+                name="use_localization",
+                default_value="True",
+                description="Use EKF to estimagte odom->base_link transform from IMU + wheel odometry",
+            ),
+            DeclareLaunchArgument(
+                "gz_verbosity",
+                default_value="3",
+                description="Verbosity level for Ignition Gazebo (0~4).",
+            ),
+            DeclareLaunchArgument(
+                "gz_args",
+                default_value="",
+                description="Extra args for Gazebo (ie. '-s' for running headless)",
+            ),
+            DeclareLaunchArgument(
+                name="log_level",
+                default_value="warn",
+                description="The level of logging that is applied to all ROS 2 nodes launched by this script.",
+            ),
+            DeclareLaunchArgument(
+                name="world",
+                default_value=default_world_path,  
+                description="Absolute path to the world file to load in Ignition Gazebo",
+            ),
+            bridge,
+            robot_state_publisher_node,
+            # robot_state_publisher_node_trailer,
+            spawn_entity,
+            robot_localization_node,
+            rviz_node,
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_entity,
+                    on_exit=[load_joint_state_controller],
+                )
+            ),
+            # RegisterEventHandler(
+            #     event_handler=OnProcessExit(
+            #         target_action=load_joint_state_controller,
+            #         on_exit=[load_joint_trajectory_controller],
+            #     )
+            # ),
+            joint_state_publisher_gui_node,
+            map_static_tf,
+            #relay_odom,
+            #relay_cmd_vel,
+        ] + gazebo
+    )
