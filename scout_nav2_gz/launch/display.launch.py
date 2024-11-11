7816a96458423de6adc2ab5e30829ede28770494
Rafael Carvalho <rafascarvalho1211@gmail.com>
2024-11-11 18:37:35 +0000

    robot localization node was messing with navigation, disabled it for now

--- scout_nav2_gz/launch/display.launch.py
+++ scout_nav2_gz/launch/display.launch.py
@@ -302,7 +302,7 @@ def generate_launch_description():
             robot_state_publisher_node,
             # robot_state_publisher_node_trailer,
             spawn_entity,
-            robot_localization_node,
+            #robot_localization_node,
             rviz_node,
             RegisterEventHandler(
                 event_handler=OnProcessExit(
