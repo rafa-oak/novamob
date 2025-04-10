cb7a67777d2a8865b1e548954112d7ea64011c7e
Rafael Carvalho <rafascarvalho1211@gmail.com>
2025-04-10 15:33:18 +0100

    Removed novamob trailer front wheels. Cleaned up URDF file

--- scout_nav2_gz/launch/display.launch.py
+++ scout_nav2_gz/launch/display.launch.py
@@ -48,24 +48,6 @@ def generate_launch_description():
     world_path = LaunchConfiguration("world")  
 
 
-    # robot_state_publisher_node = Node(
-    #     condition=IfCondition(PythonExpression(["'", use_trailer, "' == 'False'"])),
-    #     package="robot_state_publisher",
-    #     executable="robot_state_publisher",
-    #     parameters=[
-    #         {"robot_description": Command(["xacro ", LaunchConfiguration("model")])}
-    #     ],
-    # )
-
-    # robot_state_publisher_node_trailer = Node(
-    #     condition=IfCondition(PythonExpression(["'", use_trailer, "' == 'True'"])),
-    #     package="robot_state_publisher",
-    #     executable="robot_state_publisher",
-    #     parameters=[
-    #         {"robot_description": Command(["xacro ", trailer_model_path])}
-    #     ],
-    # )
-
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -305,13 +287,7 @@ def generate_launch_description():
                 default_value=default_world_path,  
                 description="Absolute path to the world file to load in Ignition Gazebo",
             ),
-            bridge,
-            image_bridge,
             robot_state_publisher_node,
-            # robot_state_publisher_node_trailer,
-            spawn_entity,
-            robot_localization_node,
-            rviz_node,
             RegisterEventHandler(
                 event_handler=OnProcessExit(
                     target_action=spawn_entity,
@@ -325,6 +301,11 @@ def generate_launch_description():
                 )
             ),
             joint_state_publisher_gui_node,
+            bridge,
+            image_bridge,
+            spawn_entity,
+            robot_localization_node,
+            rviz_node,
             relay_odom,
             relay_cmd_vel,
         ] + gazebo
